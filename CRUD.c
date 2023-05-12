e6ade32a5463c869e86023fe9e0cbe7eebb0eb5a
Het0512 <106014443+Het0512@users.noreply.github.com>
2023-05-12 14:32:38 +0900

    Update CRUD.c
    지출만 있어서 수입도 추가하고 그에 맞는 코드 수정

--- CRUD.c
+++ CRUD.c
@@ -6,6 +6,16 @@ typedef struct{
   int month, day;
 } product;
 
+/* 수입(+)인지 지출(-)인지 구분하는 변수 sign추가
+typedef struct{
+  int count;
+  int price;
+  char name[100];
+  char sign;
+  int month, day;
+} product;
+*/
+
 int menu(){
   int menu;
   printf("========== Menu ===========\n");
@@ -38,3 +48,18 @@ int createLog(product *p,int count){
   scanf("%d",&p[count].day);
   printf("\n");
 }
+
+/* 가계부에 지출만 있지 않으니 수입인지 지출인지 물어보는 코드 추가
+int createLog(product *p,int count){
+  printf("지출 내역 이름 :");
+  scanf("%[^\n]s",p[count].name);
+  printf("수입 or 지출 (수입이면 +, 지출이면 -) : ");
+  scanf("%c",&p[count].sign);
+  printf("가격 :");
+  scanf("%d",&p[count].price);
+  printf("날짜(월):");
+  scanf("%d",&p[count].month);
+  printf("날짜(일):");
+  scanf("%d",&p[count].day);
+}
+*/
