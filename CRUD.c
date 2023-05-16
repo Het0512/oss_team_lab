17daed177c90e0c611bd57dfcb4c55b2769e1c16
Het0512 <106014443+Het0512@users.noreply.github.com>
2023-05-16 12:00:34 +0900

    Update CRUD.c
    1. createLog에 있는 printf에 지줄, 수입의 숫자가 같아 수입을 1로 바꿈
    2. readLog 코드가 잘못되어 있어 코드 수정함
    3. updateLog 만듦
    4. searchLog에서 찾지 못할 경우 print할 것 추가함
    5. deletLog를 다른 버전으로 주석처리로 추가함

--- CRUD.c
+++ CRUD.c
@@ -29,7 +29,7 @@ int createLog(product *p,int count){
   int order, check = 0;
   p[count].number = count;
   p[count].sign = -1;
-  printf("입력: 지출(0) / 수입(0)");
+  printf("입력: 지출(0) / 수입(1)");
   scanf("%d",&order);
   p[count].sign = order;
   if (order == 0){
@@ -95,30 +95,106 @@ int createLog(product *p,int count){
     }
   }
 }
+
 void readLog(product *p,int count){
-  for(int i = 1; i <= count; i++){
-    int listn = count - 1;
-    if(p[count].sign == 0){
-      printf("===================\n");
-      printf("지출명: %s, 가격: - %d원, 날짜 : %d월 %d일\n",p[listn].name,p[listn].price,p[listn].month,p[listn].day);
-      printf("===================\n");
-    }
-    else if(p[count].sign == 1){
-      printf("===================\n");
-      printf("수입명: %s, 가격: + %d원, 날짜 : %d월 %d일\n",p[listn].name,p[listn].price,p[listn].month,p[listn].day);
-      printf("===================\n");
-    }
-    else if(p[count].sign == -1){
+  printf("===================\n");
+  for(int i = 0; i <= count; i++){
+    if(p[i].sign == -1){
       continue;
+    }
+    printf("%d. ", i+1);
+    if(p[i].sign == 0){
+      printf("지출명: %s, 가격: - %d원, 날짜 : %d월 %d일\n",p[i].name,p[i].price,p[i].month,p[i].day);
+    }
+    else if(p[i].sign == 1){
+      printf("수입명: %s, 가격: + %d원, 날짜 : %d월 %d일\n",p[i].name,p[i].price,p[i].month,p[i].day);
+    }
+  }
+  printf("===================\n");
+}
+
+void updateLog(product *p, int count){ // 지출, 수입은 바꿀 수 없고, 이름, 월, 일만 바꿀 수 있게
+  int num, check = 0;
+  readLog(p, count);
+  printf("수정할 가계부의 번호를 입력해주세요 : ");
+  scanf("%d", &num);
+  if(p[num-1].sign == -1){
+    printf("수정할 번호에 내용이 없습니다.\n");
+  }
+  else{
+    if (p[num-1].sign == 0){
+      printf("지출 내역 이름 :");
+    }
+    else if (p[num-1].sign == 1){
+      printf("수입 내역 이름 :");
+    }
+    scanf("%[^\n]s",p[num-1].name);
+    printf("가격 :");
+    scanf("%d",&p[num-1].price);
+    while (1){
+      printf("날짜(월):");
+      scanf("%d",&p[num-1].month);
+      if(p[num-1].month > 12 || p[num-1].month < 1){
+          printf("1~12사이로 다시 입력해주세요!!!\n");
+          continue;
+      }
+      else{
+          break;
+      }
+    }
+    while (1){
+      printf("날짜(일):");
+      scanf("%d",&p[num-1].day);
+      switch(p[num-1].month){
+          case 1:
+          case 3:
+          case 5:
+          case 7:
+          case 8:
+          case 10:
+          case 12:
+              if(p[num-1].day > 31 || p[num-1].day < 1){
+                  printf("1~31사이로 다시 입력해주세요!!!\n");
+              }
+              else{
+                  check = 1;
+              }
+              break;
+          case 2:
+              if(p[num-1].day > 28 || p[num-1].day < 1){
+                  printf("1~28사이로 다시 입력해주세요!!!\n");
+              }
+              else{
+                  check = 1;
+              }
+              break;
+          case 4:
+          case 6:
+          case 9:
+          case 11:
+              if(p[num-1].day > 30 || p[num-1].day < 1){
+                  printf("1~30사이로 다시 입력해주세요!!!\n");
+              }
+              else{
+                  check = 1;
+              }
+              break;
+      }
+      if(check = 1){
+          break;
+      }
     }
   }
 }
 
 void searchLog(product *p,int count){
-  char names[100];
+  char names[100], check = 0;;
   printf("찾고자 하는 가계부 내용의 이름을 입력해주세요 :");
   scanf("%[^\n]s",names);
   for(int i = 0; i != count; i++){
+    if(p[i].sign == -1){
+      continue;
+    }
     if (strcmp(p[i].name,names) == 0){
       if(p[i].sign == 0){
         printf("===================\n");
@@ -130,7 +206,11 @@ void searchLog(product *p,int count){
         printf("수입명: %s, 가격: + %d원, 날짜 : %d월 %d일\n",p[i].name,p[i].price,p[i].month,p[i].day);
         printf("===================\n");
       }
+      check = 1;
     }
+  }
+  if(check == 0){
+    printf("찾고자 하는 가계부 내용의  이름이 없습니다!\n");
   }
 }
 
@@ -149,3 +229,15 @@ void deleteLog(product *p,int count){
   p[number-1].sign = -1; //sign이 -1일때 삭제로 처리.(read에서 -1은 continue 처리)
   printf("삭제됨!\n");
 } 
+
+/* deletLog의 다른 버전
+void deleteLog(product *p,int count){
+  int number;
+  readLog(p, count);
+  printf("삭제하실 가계부 내역의 번호를 입력해주세요 :");
+  scanf("%d",&number);
+  printf("\n");
+  p[number-1].sign = -1; //sign이 -1일때 삭제로 처리.(read에서 -1은 continue 처리)
+  printf("삭제됨!\n");
+} 
+*/
