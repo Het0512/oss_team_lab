9ee9f5cafec7c4b7597fe2e5e56f56b546f3c57c
Het0512 <106014443+Het0512@users.noreply.github.com>
2023-05-18 22:28:04 +0900

    Update CRUD.h

--- CRUD.h
+++ CRUD.h
@@ -8,3 +8,14 @@ typedef struct{
   int sign;
   int month, day;
 } product;
+
+int createLog(Product *p); // 가계부에 내용을 추가하는 함수
+void readLog(Product *p); // 가계부의 내용 출력 함수
+int updateLog(Product *p); // 가계부 내용을 수정하는 함수
+int deleteLog(Product *p); // 가계부 내용을 삭제하는 함수
+void saveLog(Product *p, int count); // 가계부의 내용을 저장하는 함수
+int loadLog(Product *p);// 저장된 내용을 불러오는 함수
+int Menu(); // 메뉴를 불러오고 리턴을 가져오는 함수
+void searchLog(Product *p, int count); // 가계부의 내용을 검색하여 출력하는 함수
+int totalLog(Product *p); // 가계부의 적힌 총액을 계산하는 함수
+int TotalMonth(Product *p, int month); // 가계부의 적힌 내용을 기반으로 월마다 총 소비액을 출력하는 함수
