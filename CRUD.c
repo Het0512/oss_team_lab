750daab3f96512a8ffb082346a9b0fe1236c48a3
Het0512 <106014443+Het0512@users.noreply.github.com>
2023-05-22 18:35:16 +0900

    Update CRUD.c

--- CRUD.c
+++ CRUD.c
@@ -309,7 +309,7 @@ void totalLog(product *p, int count){
 int TotalMonth(product *p, int month, int count){
   int totalspend = 0;
   for(int i = 0; i < count; i++){
-    if((p[i].sign == -1)&&(p[i].month == month))
+    if(p[i].sign == -1)
       continue;
     else if ((p[i].sign == 0)&&(p[i].month == month)) {
       totalspend -= p[i].price;
@@ -317,50 +317,4 @@ int TotalMonth(product *p, int month, int count){
   }
   printf("총 지출 : %d",totalspend);
   return totalspend;
-}
-int main(void) {
-  product p[100];
-  int count = 0;
-  int search_menu;
-  while(1){
-    search_menu = menu();
-    if (search_menu == 0){
-      printf("종료됨!\n");
-      break;
-    }
-    else if(search_menu == 1){
-      createLog(p,count);
-      count++;
-    }
-    else if(search_menu == 2){
-      readLog(p, count);
-    }
-    else if(search_menu == 3){
-      deleteLog(p, count);
-    }
-    else if(search_menu == 4){
-      searchLog(p, count);
-    }
-    else if(search_menu == 5){
-      updateLog(p,count);
-    }
-    else if(search_menu == 6){
-      totalLog(p, count);
-    }
-    else if(search_menu == 7){
-      int month;
-      printf("월을 입력하세요: ");
-      scanf("%d", &month);
-      ClearLineFromReadBuffer();
-      printf("\n");
-      TotalMonth(p,month,count);
-    }
-    else if(search_menu == 8){
-      saveLog(p, count);
-    }
-    else if(search_menu == 9){
-      loadLog(p, count);
-    }
-  }
-  return 0;
 }
